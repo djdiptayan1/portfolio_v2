0fcee5e18463d59a3e258791ead6e5cf5aa1c033
Diptayan Jash <djdiptayan1@gmail.com>
2025-05-10 23:51:38 +0530

    chore: Update sitemap URLs and add redirect configuration

--- next.config.mjs
+++ next.config.mjs
@@ -8,7 +8,17 @@ const nextConfig = {
         hostname: "**"
       }
     ]
+  },
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        destination: 'http://20.193.252.127:8080/:path*',
+        permanent: false
+      }
+    ]
   }
+
 };
 
 export default nextConfig;
